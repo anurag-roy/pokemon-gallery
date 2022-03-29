36471db15dbd3b9af108f310845c81cfedaf4521
Anurag Roy <anurag@2pirad.com>
2022-03-29 16:09:14 +0530

    Update styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ function BlurImage({ image }: { image: Image }) {
       target="_blank"
       className="group"
     >
-      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-2xl xl:aspect-w-7 xl:aspect-h-8">
         <Image
           alt={image.name}
           src={image.imageUrl}
@@ -51,8 +51,10 @@ function BlurImage({ image }: { image: Image }) {
           onLoadingComplete={() => setLoading(false)}
         />
       </div>
-      <h3 className="mt-4 text-sm text-gray-700">{image.genus}</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">{image.name}</p>
+      <p className="mt-4 pl-2 text-lg font-medium text-gray-900">
+        {image.name}
+      </p>
+      <h3 className="pl-2 text-sm text-gray-700">{image.genus}</h3>
     </a>
   );
 }
@@ -60,7 +62,7 @@ function BlurImage({ image }: { image: Image }) {
 export default function Gallery({ images }: { images: Image[] }) {
   return (
     <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-10">
         {images &&
           images.map((image) => <BlurImage key={image.id} image={image} />)}
       </div>
